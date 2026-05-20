22e1955a47244d590e446b3244efc9c2f05211b2
Hikmat Abdukhaligov <hikmat.abdukhaligov@americor.com>
2026-05-21 01:09:40 +0400

    chore: cleanup docker and env

--- routes/api.php
+++ routes/api.php
@@ -5,42 +5,6 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $configs = [
-        'APP_NAME'     => 'Phober',
-        'APP_ENV'      => 'local',
-        'APP_DEBUG'    => 'true',
-
-        'TELESCOPE_ENABLED' => 'true',
-
-        'LOG_CHANNEL'              => 'logstash',
-        'LOG_DEPRECATIONS_CHANNEL' => null,
-        'LOG_LEVEL'                => 'debug',
-
-        'DB_SHARED_CONNECTION' => 'mysql',
-        'DB_SHARED_HOST'       => 'db_mysql',
-        'DB_SHARED_PORT'       => '3306',
-        'DB_SHARED_DATABASE'   => 'phober_shared',
-        'DB_SHARED_USERNAME'   => 'root',
-        'DB_SHARED_PASSWORD'   => 'root',
-
-        'BROADCAST_DRIVER' => 'log',
-        'CACHE_DRIVER'     => 'file',
-        'FILESYSTEM_DISK'  => 'local',
-        'QUEUE_CONNECTION' => 'database',
-        'SESSION_DRIVER'   => 'file',
-        'SESSION_LIFETIME' => '120',
-        'MEMCACHED_HOST'   => '127.0.0.1',
-
-        'MAIL_MAILER'       => 'smtp',
-        'MAIL_HOST'         => 'mailhog',
-        'MAIL_PORT'         => 1025,
-        'MAIL_USERNAME'     => null,
-        'MAIL_PASSWORD'     => null,
-        'MAIL_ENCRYPTION'   => null,
-        'MAIL_FROM_ADDRESS' => 'hello@example.com',
-        'MAIL_FROM_NAME'    => '${APP_NAME}',
-    ];
-
     if (config('app.use_cache')) {
         $data = Cache::remember('configs_data', 300, function () {
             return DB::table('configs')->get()->pluck('value', 'key')->toArray();
@@ -49,5 +13,5 @@ Route::get('/', function () {
         $data = DB::connection('db_configs')->table('configs')->get()->pluck('value', 'key')->toArray();
     }
 
-    return response()->json(array_merge($configs, $data));
+    return response()->json($data);
 });
