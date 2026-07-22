16aa2c306c0778244955cb2fba45fa3de96b4ab4
Hikmat Abdukhaligov <hikmat@tscinc.net>
2026-07-22 15:12:12 +0400

    improve: db connections

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,16 @@
 <?php
 
+use App\Models\Config;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     if (config('app.use_cache')) {
         $data = Cache::remember('configs_data', 300, function () {
-            return DB::table('configs')->get()->pluck('value', 'key')->toArray();
+            return Config::query()->get()->pluck('value', 'key')->toArray();
         });
     } else {
-        $data = DB::connection('db_configs')->table('configs')->get()->pluck('value', 'key')->toArray();
+        $data = Config::query()->get()->pluck('value', 'key')->toArray();
     }
 
     return response()->json($data);
