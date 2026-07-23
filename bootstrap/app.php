8e02781a06c6c4b892eb2614c10dea677ef17c33
Hikmat Abdukhaligov <hikmat@tscinc.net>
2026-07-24 02:29:58 +0400

    implement: exception handler - ProblemJsonHandler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,5 +14,5 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        \Phobiavr\PhoberLaravelCommon\Exceptions\ProblemJsonHandler::register($exceptions);
     })->create();
